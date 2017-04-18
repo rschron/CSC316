655cea2235f2e96e2a8c9336f762fdbda5434985
rschron <rschron@gmail.com>
2017-04-17 22:50:55 -0400

    Add files via upload

---
+++ Rider.java
@@ -0,0 +1,12 @@
+
+public class Rider extends Thread{
+	private Ride ride;
+	
+	public Rider(Ride r){
+		ride = r;
+	}
+	
+	public void run(){
+		int car = 0;
+	}
+}
---
+++ TestDriver.java
@@ -0,0 +1,9 @@
+
+public class TestDriver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
